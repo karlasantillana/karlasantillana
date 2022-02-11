1262c886154ab89679c38ee4724e123866b994ec
karlasantillana <karlasantillanaayulo@gmail.com>
2022-02-11 12:05:22 +0100

    cambio5

--- holamundo.java
+++ holamundo.java
@@ -1,11 +1,7 @@
-//Escribe un programa que muestre tu nombre por pantalla.
 public class ejercicio1 {
 
     public static void main(String[] args) {
-        System.out.println("Mi nombre es Karla");
-        System.out.println("HOLAAAAAAAAAAAAAAAAAAA");
-        System.out.println("Repositorios");
-        
+        System.out.println("Repositorio Despliegue);        
     }
     
 }
